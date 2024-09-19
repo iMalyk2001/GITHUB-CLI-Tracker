7afb0624b218cca169eda0da80ee9986fb71f01d
iMalyk2001 <imalyk.inc@gmail.com>
2024-09-19 12:06:15 +0100

    Made some changes and felt like pushing something

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GITHUB-CLI-Tracker
 
 go 1.23.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -17,35 +17,35 @@ type Searx struct {
 
 func (s *Searx) GetEvents() (string, error) {
 	params:= url.Values{}
-	params.Add("query", s.Query)
-	return s.BaseURL + "?" + params.Encode(), nil
+	params.Add("", s.Query)
+	return s.BaseURL, nil
 	
 }
 
 
 func main (){
+
+
 	var username string
 	fmt.Println("What is your Github Username?: ")
 	fmt.Scanf("%s", &username)
 	
+
 	
 	search := &Searx{
 		BaseURL: fmt.Sprintf("https://api.github.com/users/%s/events", username),
 	}
 	
-	eventsURL, err := search.GetEvents()
-	if err != nil {
-		log.Fatal("Error building the URL: ", err)
-	}
-
-	fmt.Println("GitHub Events URL:", eventsURL)
-
-	// Perform an HTTP GET request (example)
-	resp, err := http.Get(eventsURL)
+	resp, err := http.Get(search.BaseURL)
 	if err != nil {
 		log.Fatal("Error making request: ", err)
 	}
+	fmt.Println(resp)
 	defer resp.Body.Close()
 
 
+	
+
 }
+
+	
