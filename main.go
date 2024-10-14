8abe7f8cbaf7d82de645b7ad06b313bf5bc91bd3
iMalyk2001 <imalyk.inc@gmail.com>
2024-10-14 09:07:32 +0100

    Getting a favorable response now

--- main.go
+++ main.go
@@ -1,56 +1,67 @@
 package main
 
 import (
-	"net/http"
-	"net/url"
-	"log"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 )
+
+// Define the Yield struct
+type Yield struct {
+	Name  string `json:"username"`
+	Event string `json:"type"`
+	Date  string `json:"created_at"`
+}
+
+// Define the Searx struct
 type Searx struct {
 	BaseURL string
-	Query string
-
+	Query   string
 }
 
-// BuildURL builds the URL for the search query
-
+// GetEvents builds the URL for the search query (this function is not necessary for the current functionality)
 func (s *Searx) GetEvents() (string, error) {
-	params:= url.Values{}
-	params.Add("", s.Query)
+	// This function is not necessary for your current implementation
 	return s.BaseURL, nil
-	
 }
 
-
-func main (){
-
-
+func main() {
 	var username string
 	fmt.Println("What is your Github Username?: ")
 	fmt.Scanf("%s", &username)
-	
 
-	
+	// Create a Searx instance with the GitHub events API URL
 	search := &Searx{
 		BaseURL: fmt.Sprintf("https://api.github.com/users/%s/events", username),
 	}
-	
+
+	// Make the HTTP GET request
 	resp, err := http.Get(search.BaseURL)
 	if err != nil {
 		log.Fatal("Error making request: ", err)
 	}
-	fmt.Println(resp)
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        panic(err)
-    }
+	// Read the response body
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading response body: ", err)
+	}
 
+	// Define a slice of Yield to hold the unmarshaled data
+	var response []Yield
 
-	fmt.Println(string(body))
+	// Unmarshal the JSON data into the slice of Yield structs
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		log.Fatal("Error unmarshaling JSON: ", err)
+	}
 
+	// Print the unmarshaled data
+	fmt.Println("Events:")
+	for _, event := range response {
+		fmt.Printf("Actor: %s, Type: %s, Date: %s\n", event.Name, event.Event, event.Date)
+	}
 }
-
-	
